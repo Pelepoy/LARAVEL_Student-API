9e3cfa149a69894ebf31474492b8e7c924f9bd4c
Pelepoy <philipmarcel.maratas@cvsu.edu.ph>
2024-01-08 01:24:59 +0800

    Refactor, Add Logic for show, update and delete method.

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\Student;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Dotenv\Exception\ValidationException;
 
 class StudentController extends Controller
 {
@@ -13,12 +14,12 @@ class StudentController extends Controller
      */
     public function index()
     {
-        $students = Student::all();
+        $students = Student::orderBy('id', 'desc')->get();
 
         if ($students->isEmpty()) {
             return response()->json([
                 'success'  => false,
-                'message'  => 'Student List Empty',
+                'message'  => 'No records found',
                 'students' => []
             ], 200);
         } else {
@@ -35,7 +36,26 @@ class StudentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'name'  => 'required|max:255|regex:/^[a-zA-Z\s]+$/',
+            'course' => 'required|max:255',
+            'email' => 'required|email|unique:students,email',
+            'phone' => 'required|digits:11|unique:students,phone'
+        ]);
+
+        $student = Student::create($validated);
+
+        if ($student) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Student created successfully',
+            ], 201);
+        } else {
+            return response()->json([
+                'success' => false,
+                'error' => 'Student creation failed',
+            ], 422);
+        }
     }
 
     /**
@@ -43,7 +63,20 @@ class StudentController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $student = Student::find($id);
+
+        if ($student) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Student details',
+                'student' => $student
+            ], 200);
+        } else {
+            return response()->json([
+                "success" => false,
+                "message" => "No student records found",
+            ], 404);
+        }
     }
 
     /**
@@ -51,7 +84,28 @@ class StudentController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $student = Student::find($id);
+
+        $validated = $request->validate([
+            'name'  => 'sometimes|max:255|regex:/^[a-zA-Z\s]+$/',
+            'course' => 'sometimes|max:255',
+            'email' => 'sometimes|email|unique:students,email',
+            'phone' => 'sometimes|digits:11|unique:students,phone'
+        ]);
+
+        if ($validated) {
+            $student->update($validated);
+            return response()->json([
+                'success' => true,
+                'message' => 'Student updated successfully',
+                'student' => $student
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Student update failed'
+            ], 422);
+        }
     }
 
     /**
@@ -59,6 +113,14 @@ class StudentController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $student = Student::find($id);
+
+        if ($student) {
+            $student->delete();
+            return response()->json([
+                'success' => true,
+                'message' => 'Student with id of ' . $id . ' deleted successfully',
+            ], 204);
+        }
     }
 }
